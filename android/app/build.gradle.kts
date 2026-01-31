3ef3aa51fb249fdda6679bf50e82c764a71f7dec
desmond009 <ragnarviju2004@gmail.com>
2026-01-31 16:21:41 +0530

    Change in. Gradle file

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.example.riverpod_codegen"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -27,7 +27,8 @@ android {
         applicationId = "com.example.riverpod_codegen"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
+        ndkVersion = "27.0.12077973"
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
